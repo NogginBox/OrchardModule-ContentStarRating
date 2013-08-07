67cbb4331c07dc2a790051dad3a303199db2ec6a
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-08-07 15:31:45 +0100

    Working on rating editor view.

--- Drivers/ContentRatingDriver.cs
+++ Drivers/ContentRatingDriver.cs
@@ -1,4 +1,5 @@
 ﻿using NogginBox.ContentStarRating.Models;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
 
 namespace NogginBox.ContentStarRating.Drivers
@@ -10,6 +11,23 @@ namespace NogginBox.ContentStarRating.Drivers
 			return ContentShape("Parts_ContentRating",
 				() => shapeHelper.Parts_ContentRating(
 					Rating: part.Rating));
+		}
+
+		//GET
+		protected override DriverResult Editor(ContentRatingPart part, dynamic shapeHelper)
+		{
+			return ContentShape("Parts_ContentRating_Edit",
+				() => shapeHelper.EditorTemplate(
+					TemplateName: "Parts/ContentRating",
+					Model: part,
+					Prefix: Prefix));
+		}
+
+		//POST
+		protected override DriverResult Editor(ContentRatingPart part, IUpdateModel updater, dynamic shapeHelper)
+		{
+			updater.TryUpdateModel(part, Prefix, null, null);
+			return Editor(part, shapeHelper);
 		}
 	}
 }
